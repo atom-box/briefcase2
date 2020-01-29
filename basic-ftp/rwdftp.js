140bf739d95c8f3205731f9fa7c7ccab563e8512
Evan Genest <egenest@officesupply.com>
2020-01-29 12:40:39 -0600

    Restore point for promise-hanging-shared with Prassad.

--- basic-ftp/rwdftp.js
+++ basic-ftp/rwdftp.js
@@ -1,60 +1,64 @@
+// Command line syntax examples are at the bottom of this file
+
+// use Node 10.18.1 per Ray
+// const readline = require('readline');
+const ftp = require("basic-ftp");
 require ('dotenv').config();
 
-// for confirming delete function during testing
-const readline = require('readline');
-const ftp = require("basic-ftp");
-
 const remotePathPrefix = '/Development/';
-
 const connSettings = {
             host: process.env.OFFSUP_HOST,
             user: process.env.OFFSUP_USER,
             password: process.env.OFFSUP_PASS,
             secure: false, //true
             // use NODE options https://nodejs.org/api/tls.html#tls_tls_connect_options_callback
-
 }
 
 
-if ('undefined' === process.argv[2] || !['la', 'ln', 'lo', 'lp', 'lr', 'pu', 'ge', 'de'].includes(process.argv[2]) ){
+if ('undefined' === process.argv[2] || !['la', 'ln', 'lo', 'lp', 'lr', 'pu', 'ge', 'ds', 'dn'].includes(process.argv[2]) ){
     process.argv[2] = 'la';
 }
 
 
 switch (process.argv[2]) {
 case 'la': 
-    console.log('List all remote files. '  );
+    //List all remote files 
     listAllFiles();
     break;
 case 'ln': 
-    console.log('List remote files newer than... ' + process.argv[3] );
-    console.log('Using formats (even partially) similar to "2015-03-25" \n"03/25/2015" \n"Mar 25 2015" \n"25 Mar 2015"')
+    //List remote files newer than... ' + process.argv[3]  
+    //Using formats (even partially) similar to "2015-03-25" \n"03/25/2015" \n"Mar 25 2015" \n"25 Mar 2015"')
     listNewerFiles(process.argv[3]);  /////////// todo needs second arg
     break;
 case 'lo': 
-    console.log('List remote files older than... '  );
-    console.log('Using formats (even partially) similar to "2015-03-25" \n"03/25/2015" \n"Mar 25 2015" \n"25 Mar 2015"')
+    //List remote files older than... '  );
+    //Using formats (even partially) similar to "2015-03-25" \n"03/25/2015" \n"Mar 25 2015" \n"25 Mar 2015"')
     listOlderFiles(process.argv[3]);
     break;
 case 'lp': 
-    console.log('List remote files matching this pattern '  );
+    //List remote files matching this pattern
     listNamedFiles(process.argv[3]);
     break;
 case 'lr': 
-    console.log('List remote files as raw object '  );
+    //List remote files as raw object
     listRawFileObject();
     break;
 
 case 'pu':
-    console.log('Put file pathA to pathB.');
+    //Put file pathA to pathB.
     put(process.argv[3], process.argv[4]);
     break;
 case 'ge':
-    console.log('Get file; localA from remoteB'  );
+    // Get file; localA from remoteB
     get(process.argv[3], process.argv[4]);
     break;
-case 'de':
+case 'ds':
+    // Delete remote file by name
     remoteDelete(process.argv[3]);
+    break;
+case 'dn':
+    // Delete all files newer than...
+    deleteNewerThan(process.argv[3]);
     break;
 default:
     console.log(`Should never see this.` + process.argv[2] );// todo
@@ -64,7 +68,7 @@ default:
 async function listAllFiles() {
     const client = new ftp.Client();
     client.ftp.verbose = false;
-    try {
+    try {   
         await client.access(connSettings);
         const list = await client.list(remotePathPrefix);
         const regex1 = /\w+\ \d+\ \d+\ \d+:\d+/i; 
@@ -148,6 +152,57 @@ async function listNewerFiles(filter) {
 
 
 
+ 
+async function arrayitizeNewerFiles(filter) {
+    const client = new ftp.Client();
+    if (undefined === filter   ){
+        console.log('Please enter a date string');
+        return;
+    }
+    let cutOffMS = Date.parse(filter);
+    client.ftp.verbose = false;
+
+    try {
+        await client.access(connSettings);
+        const list = await client.list(remotePathPrefix);
+        const regex1 = /\w+\ \d+\ \d+\ \d+:\d+/i; 
+        let i = list.length - 1, 
+        d = new Date(),
+        n = '',
+        fileMSSince1970 = 0;
+        for (; i >= 0; i--){
+            fileMSSince1970 = Date.parse(list[i].modifiedAt);
+            d =  list[i].modifiedAt;
+            // console.log(`${fileMSSince1970} vs cutoff: ${cutOffMS} `)
+            if (fileMSSince1970 < cutOffMS){
+                continue;
+            }
+            d = regex1.exec(d);
+            s =  list[i].size;
+            if (list[i].type === 2) { s = 'dir'}
+            n = list[i].name 
+            console.log('\t' + d 
+                + '\t' + s
+                + '\t' + n);
+        }
+    }
+    catch(err) {
+        console.log(err);
+    }
+    return ['lorem', 'ipsum', 'foo', 'baz', 'bar'];
+    client.close();
+}
+
+
+function deleteNewerThan(filter) {
+    // console.log(`DEL newer than received ${filter} at ${new Date()}`);
+    const newishFileNames = arrayitizeNewerFiles(filter);
+    console.log('Returned an array with this many items: ' + newishFileNames.length);
+    console.dir('The array is this: ' + newishFileNames);
+}
+
+
+
 async function listNamedFiles(filter) {
     const client = new ftp.Client();
     if (undefined === filter   ){
@@ -164,8 +219,7 @@ async function listNamedFiles(filter) {
         regex2 = new RegExp(filter, 'i'); 
         let i = list.length - 1, 
         d = new Date(),
-        n = '',
-        fileMSSince1970 = 0;
+        n = '';
         for (; i >= 0; i--){
             d =  list[i].modifiedAt;
             if (!regex2.test(list[i].name)){
@@ -190,8 +244,6 @@ async function listNamedFiles(filter) {
 
 
 
- 
- 
 async function put(){
     const client = new ftp.Client();
     client.ftp.verbose = true;
@@ -228,7 +280,7 @@ async function remoteDelete(remoteFilename){
     try {
         await client.access(connSettings);
         const completeRemotePath = remotePathPrefix + remoteFilename;
-        console.log(`Will delete ${completeRemotePath} after a 3 second delay!`);
+        console.log(`Will delete ${completeRemotePath}`); 
         await client.remove(completeRemotePath);
     }
 
@@ -240,5 +292,23 @@ async function remoteDelete(remoteFilename){
 
 
 
-// Useage:    node uploadOnlyFtp.js toHere.ext   fromThere.ext
+/* Useage:  SYNTAX EXAMPLES for the Node.js terminal:
 
+node rwd-basic-ftp.js   de   remote.csv
+node rwd-basic-ftp.js   pu   local.csv  remote.csv
+node rwd-basic-ftp.js   ge   local.csv  remote.csv
+node rwd-basic-ftp.js   ln   "25 Jan 2020" 
+node rwd-basic-ftp.js   ln  "25 Mar 2015"
+
+The two letter options are
+1. DE  delete
+2. PU  put
+3. GE  get
+4. LN  list newer than
+5. LO  list older than
+6. LP  list by filename pattern
+7. LA  list all
+
+
+
+*/
